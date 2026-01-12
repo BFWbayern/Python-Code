6c2cbc2caaaed56466e69788e54c50d88d2b8ba6
BayerN <bayern@o365-bfw.koeln>
2026-01-12 13:30:41 +0100

    Implement 'aufgabe' 3 to 7.

--- PythonUebung/aufgaben.py
+++ PythonUebung/aufgaben.py
@@ -1,3 +1,5 @@
+from helpers.io_helper import request_input
+
 def aufgabe1():
     print("Hello, World!")
 
@@ -5,3 +7,27 @@ def aufgabe2():
     print("Wie ist dein Vorname?")
     vorname = input()
     print(f"Hallo, {vorname}")
+
+def aufgabe3():
+    num = 3.1415
+    print(f"{num}: {type(num)}")
+
+def aufgabe4():
+    text = 10
+    textAsInt = int(text)
+    textAsFloat = float(text)
+    print(f"{textAsInt=}")
+    print(f"{textAsFloat=}")
+
+def aufgabe5():
+    num = 3.1415
+    print(round(num, 2))
+
+def aufgabe6():
+    user_input = request_input()
+    print(f"{user_input}: {type(user_input)}")
+
+def aufgabe7():
+    user_input = request_input()
+    print(f"Length of '{user_input}': {len(user_input)}")
+
---
+++ PythonUebung/helpers/io_helper.py
@@ -0,0 +1,4 @@
+def request_input() -> str:
+    print("Tätigen Sie eine Eingabe!")
+    user_input = input()
+    return user_input
