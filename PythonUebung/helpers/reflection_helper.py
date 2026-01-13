1bbed7462c319277972cc358ae05250c201c59bd
BayerN <bayern@o365-bfw.koeln>
2026-01-13 07:51:02 +0100

    Get rid of redundand module name variable in reflection_helper.py.

--- PythonUebung/helpers/reflection_helper.py
+++ PythonUebung/helpers/reflection_helper.py
@@ -3,12 +3,11 @@ from inspect import getmembers, isfunction
 
 class ModuleReflector:
     def __init__(self, module_name):
-        self.module_name = module_name
+        self.module = import_module(module_name)
 
     def get_functions(self):
-        module = import_module(self.module_name)
         functions = [
-            func for name, func in getmembers(module, isfunction)
-            if (func.__module__ == module.__name__)
+            func for name, func in getmembers(self.module, isfunction)
+            if (func.__module__ == self.module.__name__)
         ]
         return functions
