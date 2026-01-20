d8bb4b39511748ad36f3c45c3667aee15924803c
BayerN <bayern@o365-bfw.koeln>
2026-01-20 11:35:32 +0100

    Transfer code from smartboard. Jan 20, 2026 (2)

--- PythonUebung/unterricht.py
+++ PythonUebung/unterricht.py
@@ -1,3 +1,5 @@
+from sys import maxsize
+
 def addieren(a: int, b: int) -> int:
     s = a + b
     return s
@@ -6,3 +8,15 @@ a = 10
 b = 20
 summe = addieren(a, b)
 print(summe)
+
+# ---
+
+def my_min(liste: list[int]) -> int:
+    min = maxsize
+    for i in liste:
+        if i < min:
+            min = i
+    return min
+
+daten = [10, 500, 3]
+print(my_min(daten))
