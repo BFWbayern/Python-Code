aecc1544dc40fc821c9daf0efdc7e37dad46c794
BayerN <bayern@o365-bfw.koeln>
2026-01-26 09:13:55 +0100

    Tidy up some code.

--- Hangman/console_renderer.py
+++ Hangman/console_renderer.py
@@ -3,50 +3,73 @@ def get_whitespace_list(length: int) -> list[str]:
 
 class Renderer:
     def __init__(self, width: int, height: int) -> None:
-        self.buffer = get_whitespace_list(width * height)
-        self.x = 0
-        self.y = 0
-        self.width = width
-        self.height = height
+        self.__buffer = get_whitespace_list(width*height)
+        self.__x = 0
+        self.__y = 0
+        self.__width = width
+        self.__height = height
 
     def to_str(self) -> str:
         output = ""
-        for i in range(0, len(self.buffer)):
-            if(i >= self.width and i % self.width == 0):
-                output += "\n"
-            output += self.buffer[i]
-            
+
+        for i in range(0, len(self.__buffer)):
+            if(i >= self.__width and i % self.__width == 0):
+                output += '\n'
+            output += self.__buffer[i]
         return output.rstrip()
     
-    def set_cursor(self, x: int, y: int):
-        if x >= self.width:
-            new_row = x // self.width
-            x = x - self.width * new_row
+    def __str__(self):
+        return self.to_str()
+
+    def get_x(self) -> int:
+        return self.__x
+
+    def get_y(self) -> int:
+        return self.__y
+    
+    def get_width(self) -> int:
+        return self.__width
+
+    def get_height(self) -> int:
+        return self.__height
+    
+    def set_cursor(self, x: int, y: int) -> None:
+        if(x >= self.__width):
+            new_row = x // self.__width
+            x = x - self.__width*new_row
             y = y + new_row
-        if y >= self.height:
-            raise Exception("The rendering buffer is too small.")
-        self.x = x
-        self.y = y
 
-    def put(self, char: str):
-        if type(char) is not str:
+        if(y >= self.__height):
+            raise Exception("The rendering buffer is too small.")
+        self.__x = x
+        self.__y = y
+
+    def set_x(self, x: int) -> None:
+        self.set_cursor(x, self.get_y())
+
+    def set_y(self, y: int) -> None:
+        self.set_cursor(self.get_x(), y)
+
+    def put(self, char: str) -> None:
+        if(type(char) is not str):
             char = str(char)
-        if len(char) != 1:
-            raise Exception(f"Parameter 'char' must be of length 1, actual length: {len(char)}.")
-        index = self.x + self.y * self.width
-        if index >= len(self.buffer):
-            raise Exception("The rendering buffer is too small.")
 
-        self.buffer[index] = char
+        if(len(char) != 1):
+            raise Exception(f"Parameter 'char' must be of length 1, actual length: {len(char)}.")
+        index = self.__x + self.__y*self.__width
+
+        if(index >= len(self.__buffer)):
+            raise Exception("The rendering buffer is too small.")
+        self.__buffer[index] = char
         self.indent()
 
-    def put_for(self, char: str, length: int):
+    def put_for(self, char: str, length: int) -> None:
         for _ in range(length):
             self.put(char)
 
-    def insert(self, text: str):
+    def insert(self, text: str) -> None:
         for char in text:
             self.put(char)
 
-    def indent(self, i: int = 1):
-        self.set_cursor(self.x + i, self.y)
+    def indent(self, i: int = 1) -> None:
+        self.set_cursor(self.__x + i, self.__y)
--- Hangman/program.py
+++ Hangman/program.py
@@ -3,57 +3,59 @@ import os
 import time
 from console_renderer import Renderer
 
-THE_WORD = "The word: "
+THE_WORD_STR = "The word: "
+THE_WORD_LEN = len(THE_WORD_STR)
 
 class Game:
     def __init__(self) -> None:
         self.is_running = False
-        self.won = False
+        self.is_won = False
         self.words = ["Testsubjekt", "Willkuerlich", "Sinnlos", "Zufaellig"]
         self.index = random.randint(0, len(self.words)-1)
         self.word = self.words[self.index]
-        self.discovered:list[str] = [self.word[random.randint(0, len(self.word)-1)].lower()]
-        self.wrong_guesses:list[str] = []
-        self.MAX_WIDTH = len(THE_WORD) + len(self.word)*2+3
+        self.discovered: list[str] = [self.word[random.randint(0, len(self.word)-1)].lower()]
+        self.wrong_guesses: list[str] = []
+        self.MAX_WIDTH = THE_WORD_LEN + len(self.word)*2 + 3
         self.MAX_HEIGHT = 7
     
-    def renderer(self):
-        renderer = Renderer((self.MAX_WIDTH)*2, self.MAX_HEIGHT)
+    def render(self) -> None:
+        renderer = Renderer(self.MAX_WIDTH*2, self.MAX_HEIGHT)
         self._render_box(renderer)
         self._render_guess_screen(renderer)
         self._render_hangman_screen(renderer)
-        os.system('cls')
-        print(renderer.to_str())
+        os.system("cls")
+        print(str(renderer))
 
-    def _render_box(self, renderer: Renderer):
+    def _render_box(self, renderer: Renderer) -> None:
         renderer.put('╔')
         renderer.put_for('═', self.MAX_WIDTH-2)
         renderer.put('╦')
         renderer.put_for('═', self.MAX_WIDTH-2)
         renderer.put('╗')
         
-        for i in range(1,self.MAX_HEIGHT):
+        for i in range(1, self.MAX_HEIGHT):
             renderer.set_cursor(0, i)
             renderer.put('║')
             renderer.set_cursor(self.MAX_WIDTH-1, i)
             renderer.put('║')
-            renderer.set_cursor(self.MAX_WIDTH*2-2, i)
+            renderer.set_cursor(self.MAX_WIDTH*2 - 2, i)
             renderer.put('║')
         
-        renderer.set_cursor(0,self.MAX_HEIGHT-1)
+        renderer.set_cursor(0, self.MAX_HEIGHT-1)
         renderer.put('╚')
         renderer.put_for('═', self.MAX_WIDTH-2)
         renderer.put('╩')
         renderer.put_for('═', self.MAX_WIDTH-2)
         renderer.put('╝')
     
-    def _render_guess_screen(self, renderer: Renderer):
-        renderer.set_cursor(2,1)
+    def _render_guess_screen(self, renderer: Renderer) -> None:
+        renderer.set_cursor(2, 1)
         renderer.insert("The word: ")
-        i = len(THE_WORD) - 1
+        i = THE_WORD_LEN-1
+
         for char in self.word:
-            if (self.discovered.__contains__(char.lower())):
-                renderer.insert(char+ ' ')
+            if(char.lower() in self.discovered):
+                renderer.insert(char + ' ')
             else:
                 renderer.insert("_ ")
             i += 2
@@ -61,116 +63,141 @@ class Game:
         WRONG_GUESSES_STR = "Wrong guesses: "
         i = len(WRONG_GUESSES_STR)
         y = 2
+
         renderer.set_cursor(2, y)
         renderer.insert(WRONG_GUESSES_STR)
+
         if(len(self.wrong_guesses) > 0):
-            last_disc = self.wrong_guesses[-1]
-            for disc in self.wrong_guesses:
+            last_wrong_guess = self.wrong_guesses[-1]
+            for wrong_guess in self.wrong_guesses:
                 if(i >= self.MAX_WIDTH-2-3):
                     y += 1
-                    renderer.set_cursor(2,y)
-                    i=0
-                renderer.insert(disc)
-                i+=1
-                if(disc != last_disc):
+                    i = 0
+                    renderer.set_cursor(2, y)
+                i += 1
+                renderer.insert(wrong_guess)
+                if(wrong_guess != last_wrong_guess):
+                    i += 2
                     renderer.insert(", ")
-                    i+=2
         else:
-            renderer.put_for('.', 3)
+            renderer.insert("...")
 
-    def _render_hangman_screen(self, renderer: Renderer):
-        wrong_count = len(self.wrong_guesses)
-        if(wrong_count < 1):
+    def _render_hangman_screen(self, renderer: Renderer) -> None:
+        wrong_guesses_count = len(self.wrong_guesses)
+
+        if(wrong_guesses_count < 1):
             return
-        renderer.set_cursor(int(self.MAX_WIDTH*1.5-4), self.MAX_HEIGHT-2)
-        renderer.insert('━┻━')
-        if(wrong_count < 2):
+        renderer.set_cursor(int(self.MAX_WIDTH*1.5 - 4), self.MAX_HEIGHT-2)
+        renderer.insert("━┻━")
+
+        if(wrong_guesses_count < 2):
             return
-        renderer.set_cursor(renderer.x-2,renderer.y-1)
+        renderer.set_cursor(renderer.get_x()-2, renderer.get_y()-1)
         renderer.put('┃')
-        if(wrong_count < 3):
+
+        if(wrong_guesses_count < 3):
             return
-        renderer.set_cursor(renderer.x-1,renderer.y-1)
+        renderer.set_cursor(renderer.get_x()-1, renderer.get_y()-1)
         renderer.put('┃')
-        if(wrong_count < 4):
+
+        if(wrong_guesses_count < 4):
             return
-        renderer.set_cursor(renderer.x-1,renderer.y-1)
+        renderer.set_cursor(renderer.get_x()-1, renderer.get_y()-1)
         renderer.put('┃')
-        renderer.set_cursor(renderer.x-1,renderer.y-1)
+        renderer.set_cursor(renderer.get_x()-1, renderer.get_y()-1)
         renderer.put('┏')
-        if(wrong_count<5):
+
+        if(wrong_guesses_count < 5):
             return
-        renderer.insert('━━')
-        if(wrong_count<6):
+        renderer.insert("━━")
+
+        if(wrong_guesses_count < 6):
             return
-        renderer.insert('━┑')
-        if(wrong_count < 7):
+        renderer.insert("━┑")
+
+        if(wrong_guesses_count < 7):
             return
-        renderer.set_cursor(renderer.x-1,renderer.y+1)
+        renderer.set_cursor(renderer.get_x()-1, renderer.get_y()+1)
         renderer.put('0')
-        if(wrong_count<8):
+
+        if(wrong_guesses_count < 8):
             return
-        renderer.set_cursor(renderer.x-2,renderer.y+1)
+        renderer.set_cursor(renderer.get_x()-2, renderer.get_y()+1)
         renderer.insert('/│\\')
-        if(wrong_count<9):
+
+        if(wrong_guesses_count < 9):
             return
-        renderer.set_cursor(renderer.x-3,renderer.y+1)
+        renderer.set_cursor(renderer.get_x()-3, renderer.get_y()+1)
         renderer.insert('/ \\')
 
-    def is_won(self) -> bool:
+    def check_is_won(self) -> bool:
         for char in self.word:
             if (not self.discovered.__contains__(char.lower())):
                 return False
         return True
 
-    def request(self):
+    def request(self) -> None:
         user_input = input("Eingabe: ").lower()
-        if(len(user_input)!=1):
-            self.renderer()
+
+        if(len(user_input) != 1):
+            self.render()
             print("Bitte geben Sie einen Buchstaben ein.")
             return self.request()
         char = user_input[0]
         val = ord(char)
-        if(not(val > 64 and val < 91 or val > 96 and val < 123)):
-            self.renderer()
+
+        if(not (val > 64 and val < 91 or val > 96 and val < 123)):
+            self.render()
             print("Eingabe war kein Buchstabe. Bitte erneut versuchen.")
             return self.request()
-        if(char in self.word.lower()):
-            if(not (char in self.discovered)):
+        
+        if(self.word.lower().__contains__(char)):
+            if(not self.discovered.__contains__(char)):
                 self.discovered.append(char)
                 return
             else:
-                self.renderer()
+                self.render()
                 print("Der ist schon entdeckt.")
                 return self.request()
-        if(char in self.wrong_guesses):
-            self.renderer()
+            
+        if(self.wrong_guesses.__contains__(char)):
+            self.render()
             print("Wir wissen schon, der ist es nicht.")
             return self.request()
         self.wrong_guesses.append(char)
         return
         
-    def run(self):
+    def run(self) -> "Game":
+        if(self.is_running):
+            raise Exception("The game is already running.")
+        
         self.is_running = True
-        while((self.is_running)):
-            self.renderer()
+
+        while(self.is_running):
+            self.render()
             self.request()
+
             if(len(self.wrong_guesses) == 9):
                 self.is_running = False
-            if(self.is_won()):
+                self.is_won = False
+
+            if(self.check_is_won()):
                 self.is_running = False
-                self.won = True
+                self.is_won = True
+
             if(not self.is_running):
                 break
             time.sleep(0.25)
-        self.renderer()
-        if(self.won):
+        self.render()
+
+        if(self.is_won):
             print("Gewonnen!")
         else:
             print("Verloren!")
 
-if(not (__name__ == "__main__")):
-    raise Exception("The module cannot be imported.")
+        return self
 
-game = Game()
-game.run()
+if(not __name__ == "__main__"):
+    raise Exception(f"The module \"{__name__}\" cannot be imported.")
+
+game = Game().run()
