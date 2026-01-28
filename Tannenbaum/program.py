4b571263414d04c885b68973ffc9d71892f47a4a
BayerN <bayern@o365-bfw.koeln>
2026-01-28 09:55:09 +0100

    Implement text-based ui.

--- Tannenbaum/program.py
+++ Tannenbaum/program.py
@@ -7,5 +7,15 @@ def tannenbaum(ebenen: int) -> str:
 
 if __name__ == "__main__":
     while (True):
-        eingabe = int(input("Ebenenzahl: "))
-        print(tannenbaum(eingabe))
+        user_input = input("Ebenenzahl oder q: ")
+        try:
+            ebene = int(user_input)
+
+            if(ebene < 1):
+                print("Bitte geben Sie eine Ganzzahl größer als 0 ein.")
+                continue
+            print(tannenbaum(ebene))
+        except ValueError:
+            if(len(user_input) == 1 and (user_input[0] == 'q' or user_input[0] == 'Q')):
+                break
+            print("Eingabe war keine Ganzzahl.")
